adac5c9026f5c5dc2394b9d85bbfdb046a823e89
VorosDominik <Medoxidhun@gmail.com>
2023-05-19 13:06:41 +0200

    Sakkbábuk

---
+++ Babuklista.js
@@ -0,0 +1,18 @@
+export const Sakkbábuk = [
+    //fekete
+    {szimbolumkód: "&#9823;", név: "paraszt"},
+    {szimbolumkód: "&#9820;", név: "Bástya"},
+    {szimbolumkód: "&#9822;", név: "ló"},
+    {szimbolumkód: "&#9821;", név: "futó"},
+    {szimbolumkód: "&#9818;", név: "király"},
+    {szimbolumkód: "&#9819;", név: "Királynő"},
+    //fehér
+    {szimbolumkód: "&#9817;", név: "paraszt"},
+    {szimbolumkód: "&#9814;", név: "Bástya"},
+    {szimbolumkód: "&#9816;", név: "ló"},
+    {szimbolumkód: "&#9815;", név: "futó"},
+    {szimbolumkód: "&#9812;", név: "király"},
+    {szimbolumkód: "&#9813;", név: "Királynő"},
+    
+   
+]
