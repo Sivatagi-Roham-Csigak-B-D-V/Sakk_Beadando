3dbef458c4d101d796a8333794e3f053422e1309
ASDleves <ricsi20020302@gmail.com>
2023-05-19 13:47:07 +0200

    pálya formázása

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
 import Palya from "./palya.js";
 
-$(function (){
+$(function() {
     const szuloElem = $("section");
-    let szamlalo = 0
-      for (let index = 0; index < 8; index++) {
-        this.divElem = $("section div:last-child");
-        for (let index_kett = 0; index_kett < 8; index++) {
-            if (szamlalo == 0){
-                const palya = new Palya(index_kett, szuloElem);
-                if (index_kett % 2 == 0){
-                    this.divElem.css("background-color", "brown")
-                }else{
-                    this.divElem.css("background-color", "white");
-        
-                }
-            }
-        }
+    let szamlalo = 0;
+  
+    for (let index = 1; index < 65; index++) {
+      const palya = new Palya(index, szuloElem);
+      szamlalo++;
+      if (szamlalo === 16) {
+        szamlalo = 0;
       }
-
- })
+    }
+  });
--- palya.js
+++ palya.js
@@ -1,16 +1,25 @@
-class Palya{
-    constructor(index, szuloElem){
-        this.index = index;
-        szuloElem.append(`<div class="negyzet"><p>Sakk</p></div>`)
-
-        this.divElem = $("article div:last-child");
-
-        this.pElem= this.divElem.children("TESZT")
-        this.divElem = $("article div:last-child");
-        };
-        
-
+class Palya {
+    constructor(index, szuloElem) {
+      this.index = index;
+      this.divElem = $("<div class='cella'><p>Sakk</p></div>");
+      szuloElem.append(this.divElem);
+      this.applyBackground();
     }
-
-
+  
+    applyBackground() {
+      if (this.index <= 8 || (this.index > 16 && this.index <= 24) || (this.index > 32 && this.index <= 40) || (this.index > 48 && this.index <= 56)) {
+        if (this.index % 2 === 0) {
+          this.divElem.css("background-color", "brown");
+        } else {
+          this.divElem.css("background-color", "white");
+        }
+      } else {
+        if (this.index % 2 === 0) {
+          this.divElem.css("background-color", "white");
+        } else {
+          this.divElem.css("background-color", "brown");
+        }
+      }
+    }
+  }
 export default Palya
