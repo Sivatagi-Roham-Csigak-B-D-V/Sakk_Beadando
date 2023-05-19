cae1c94389da1354acffdbeb6f5751448879a831
Mancska <franyokrisz@gmail.com>
2023-05-19 12:55:13 +0200

    infoPanel javitva

--- infoPanel.js
+++ infoPanel.js
@@ -1,27 +1,26 @@
 class Infopanel {
 
     #gyozelem
-    constructor(asideElem) {
-
-
-  
-      asideElem.append(`<p class="lepes"></p>`);
-      asideElem.append(`<p class="jatekos"></p>`);
-      asideElem.append(`<p class="gyozelem"></p>`);
-  
-      this.lepes = $(".lepes");
-      this.jatekos = $(".jatekos");
-      this.#gyozelem = $(".gyozelem");
+    init(){
+        const asideElem = document.createElement('aside')
+        asideElem.append(`<p class="lepes"></p>`);
+        asideElem.append(`<p class="jatekos"></p>`);
+        asideElem.append(`<p class="gyozelem"></p>`);
+    
+        this.lepes = $(".lepes");
+        this.jatekos = $(".jatekos");
+        this.#gyozelem = $(".gyozelem");
+      }
+    
+      setLepes(lepes) {
+        this.lepes.html(lepes).append(". lépés");
+      }
+      setJatekos(jatekos) {
+        this.jatekos.html(jatekos);
+      }
+      setVege(gyozelem) {
+        this.gyozelem.html(gyozelem);
+      }
     }
-  
-    setLepes(lepes) {
-      this.lepes.html(lepes).append(". lépés");
-    }
-    setJatekos(jatekos) {
-      this.jatekos.html(jatekos);
-    }
-    setVege(gyozelem) {
-      this.gyozelem.html(gyozelem);
-    }
-  }
+ 
   export default Infopanel;
