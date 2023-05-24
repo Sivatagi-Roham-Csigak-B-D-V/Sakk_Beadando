7417d60a31c51031b53a01aaf07512ed167a7fb5
dkornel01 <kornel.dobrocsi@gmail.com>
2023-05-24 09:35:27 +0200

    sec_save

--- main.js
+++ main.js
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
-import Palya from "./palya.js";
-import Babu from "./babu.js";
-
-$(function() {
-    const szuloElem = $("section");
-    let szamlalo = 0;
-  
-    for (let index = 1; index < 65; index++) {
-      const palya = new Palya(index, szuloElem);
-      const babu = new Babu(index)
-      szamlalo++;
-      if (szamlalo === 16) {
-        szamlalo = 0;
-      }
-    }
-  });
-=======
 import Jatekter from "./jatekter.js";
 
 $(function (){
   new Jatekter()
 })
->>>>>>> 1207a19027a457110831d1b7a65eff6d4e3f02ea
