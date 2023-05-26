9e32973e7b84c7d37aa9e8f931addeb04fbc621b
ASDleves <ricsi20020302@gmail.com>
2023-05-26 12:18:57 +0200

    infopanel teszt2.0

--- infoPanel.js
+++ infoPanel.js
@@ -1,35 +1,19 @@
-class Infopanel {
-  constructor() {
-      this.lepesElement = document.createElement('p');
-      this.lepesElement.classList.add('lepes');
-
-      this.jatekosElement = document.createElement('p');
-      this.jatekosElement.classList.add('jatekos');
-
-      this.gyozelemElement = document.createElement('p');
-      this.gyozelemElement.classList.add('gyozelem');
+class InfoPanel {
+    #lepesElem;
+  
+    constructor(szuloElem) {
+      const p1 = $("<p>").text("Játékos: X");
+      const p2 = $("<p>").text(`Lépésszám: `);
+      const p3 = $("<p>").text("Játék még nincs elkezdve");
+  
+      this.#lepesElem = p2;
+  
+      $(szuloElem).append(p1, p2, p3);
+    }
+  
+    updateLepes(lepes) {
+      this.#lepesElem.text(`Lépésszám: ${lepes}`);
+    }
   }
-
-  setLepes(lepes) {
-      this.lepesElement.textContent = `${lepes}. lépés`;
-  }
-
-  setJatekos(jatekos) {
-      this.jatekosElement.textContent = jatekos;
-  }
-
-  setgyozelem(gyozelem) {
-      this.gyozelemElement.textContent = gyozelem;
-  }
-
-  megjelenit(parentElement) {
-      const asideElem = document.createElement('aside');
-      asideElem.appendChild(this.lepesElement);
-      asideElem.appendChild(this.jatekosElement);
-      asideElem.appendChild(this.gyozelemElement);
-
-      parentElement.appendChild(asideElem);
-  }
-}
-
-export default Infopanel;
+  
+  export default InfoPanel;
--- jatekter.js
+++ jatekter.js
@@ -1,10 +1,16 @@
 import Palya from "./palya.js";
+import InfoPanel from "./infoPanel.js";
 class Jatekter {
+    #lepes;
     constructor(){
+      this.#lepes = 0;
         $(function() {
             const szuloElem = $("section");
             let szamlalo = 0;
-          
+            const asideElem = $("aside");
+            const infoPanel = new InfoPanel(asideElem);
+            const playerPTag = $(asideElem).find("p").first();
+            const jatekfolyamat = $(asideElem).find("p").last();
             for (let index = 1; index < 65; index++) {
               const id = index; // Generate a unique ID for each instance
               const palya = new Palya(index, szuloElem, id);
@@ -14,7 +20,22 @@ class Jatekter {
               }
             }
           });
-          
+          $(window).on("elemKivalasztas", (event) => {
+            if (this.#lepes % 2 === 0) {
+              event.detail.setElem("X");
+              playerPTag.text("Játékos: O");
+            } else {
+              event.detail.setElem("O");
+              playerPTag.text("Játékos: X");
+            }
+            this.#lepes++;
+            infoPanel.updateLepes(this.#lepes);
+            if (this.#lepes < 9){
+                jatekfolyamat.text("A játék folyamatban")
+            }else{
+                jatekfolyamat.text("A játék vége!")
+            }
+          });  
     }
     
 }
