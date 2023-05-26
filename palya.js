aad2793fc3e61ef55fb65196290e17a61891549f
ASDleves <ricsi20020302@gmail.com>
2023-05-26 12:12:16 +0200

    palya szinezes

--- palya.js
+++ palya.js
@@ -2,7 +2,7 @@ class Palya {
     constructor(index, szuloElem, id) {
       this.index = index;
       this.id = id;
-      this.divElem = $("<div class='cella'><p>Sakk</p></div>");
+      this.divElem = $("<div class='cella'></div>");
       this.divElem.attr("id", this.id);
       szuloElem.append(this.divElem);
       
@@ -12,15 +12,15 @@ class Palya {
     applyBackground() {
       if (this.index <= 8 || (this.index > 16 && this.index <= 24) || (this.index > 32 && this.index <= 40) || (this.index > 48 && this.index <= 56)) {
         if (this.index % 2 === 0) {
-          this.divElem.css("background-color", "brown");
+          this.divElem.css("background-color", "#A20D0D");
         } else {
-          this.divElem.css("background-color", "white");
+          this.divElem.css("background-color", "#BAB4B4");
         }
       } else {
         if (this.index % 2 === 0) {
-          this.divElem.css("background-color", "white");
+          this.divElem.css("background-color", "#BAB4B4");
         } else {
-          this.divElem.css("background-color", "brown");
+          this.divElem.css("background-color", "#A20D0D");
         }
       }
     }
