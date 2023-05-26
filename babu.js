80a1fff433646a4a6608b3c0523e50d11cd8ed24
dkornel01 <kornel.dobrocsi@gmail.com>
2023-05-26 13:12:57 +0200

    babu felallitas

--- babu.js
+++ babu.js
@@ -24,52 +24,52 @@ class Babu{
     setNev(nev){
         switch(nev){
             case "fekete_Paraszt":
-                new Paraszt("")
-                Paraszt.kod()
+                const parasztb=new Paraszt("")
+                this.nev=Paraszt.kod()
                 break;
             case "fekete_Bastya":
-                new Bastya("")
-                ParasBastyazt.kod()
+                const bastyab=new Bastya("")
+                this.nev=bastya.kod()
                 break;
             case "fekete_Futo":
-                const futo=new Futo("");
-                Futo.kod()
+                const futob=new Futo("");
+                this.nev=Futo.kod()
                 break;
             case "fekete_Ló":
-                const lo=new Lo("")
+                const lob=new Lo("")
                 this.nev=Lo.kod()
                 break;
             case "fekete_királynő":
-                const kiralyno=new Kiralyno("")
+                const kiralynob=new Kiralyno("")
                 this.nev=Kiralyno.kod()
                 break;
             case "fekete_király":
-                const kiraly=new Kiraly("")
+                const kiralyb=new Kiraly("")
                 this.nev=Kiraly.kod()
                 break;
             case "feher_Paraszt":
-                const paraszt=new Paraszt("feher")
+                const parasztf=new Paraszt("feher")
                 this.nev=paraszt.kod()
                 break;
             case "feher_Bastya":
-                new Bastya("feher")
-                Bastya.kod()
+                const bastyaf=new Bastya("feher")
+                this.nev=bastyaf.kod()
                 break;
             case "feher_Futo":
-                new Futo("feher");
-                Futo.kod()
+                const futof=new Futo("feher");
+                this.nev=futof.kod()
                 break;
             case "feher_Ló":
-                new Lo("feher")
-                Lo.kod()
+                const lof=new Lo("feher")
+                this.nev=lof.kod()
                 break;
             case "feher_királynő":
-                new Kiralyno("feher")
-                Kiralyno.kod()
+                const kiralynof=new Kiralyno("feher")
+                this.nev=kiralynof.kod()
                 break;
             case "feher_király":
-                new Kiraly("feher")
-                Kiraly.kod()
+                const kiralyf=new Kiraly("feher")
+                this.nev=kiralyf.kod()
                 break;
         }
     }
