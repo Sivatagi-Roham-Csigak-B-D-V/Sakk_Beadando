455fac1df4f58baa698656bea7246a039d93379b
dkornel01 <115635103+dkornel01@users.noreply.github.com>
2023-05-30 21:14:21 +0200

    futo_es_bastya

--- bastya.js
+++ bastya.js
@@ -10,11 +10,81 @@ class Bastya{
         return "&#9820;"
         }
      }
-    Lepesek(index){
-        
+    Lepesekf(index){
+        let szamok=[]
+        let szamlalo=index
+        while (szamlalo<64) {
+            szamok.push(szamlalo)
+            szamlalo=szamlalo+8
+        }
+        return szamok
     }
-    Lepes_Lehetoseg(){
+    lepesj(index){
+        let szamok=[]
+        let szamlalo=index
+        while (szamlalo>0) {
+            szamok.push(szamlalo)
+            szamlalo=szamlalo-8
+        }
+        return szamok
+    }
+    lepesb(index){
+        let szamok=[]
+        let szamlalo=index
+        while(Lepes_Lehetoseg(index,szamlalo)){
+            szamok.push(szamlalo)
+            szamlalo=szamlalo+1
+         }
+         return szamok
+    }
+    lepesl(index){
+        let szamok=[]
+        let szamlalo=index
+        while(szamlalo>-4 && Lepes_Lehetoseg(index,szamlalo)){
+            szamok.push(szamlalo)
+            szamlalo=szamlalo-1
+        }
+        return szamok
+    }
         
+    Lepes_Lehetoseg(index){
+        let sor=index/8
+        let viszintes=index/8==sor && index>sor*8
+        return viszintes
+    }
+    Lepes_Lehetoseg(index,szamlalo){
+        let sor=index/8
+        sor=szam(sor)
+        szamlalo=szamlalo/8
+        let viszintes=sor<=szamlalo
+        let viszintes2=sor+1>=szamlalo
+        return viszintes&& viszintes2
+    }
+    szam(index){
+        if (index>7){
+            return 7
+        }
+        if (index>6){
+            return 6
+        }
+        if (index>5){
+            return 5
+        }
+        if (index>4){
+            return 4
+        }
+        if (index>3){
+            return 3
+        }
+        if (index>2){
+            return 2
+        }
+        if (index>1){
+            return 1
+        }
+        if (index>0){
+            return 0
+        }
     }
 }
 export default Bastya
--- futo.js
+++ futo.js
@@ -10,11 +10,62 @@ class Futo{
         return "&#9821;"
         }
      }
-    Lepesek(){
+    Lepesekjf(index){
+        let szamok=[]
+        let szamlalo=hatarr(index/8)
+        if (szamlalo>hatarf(index%8)){
+            szamlalo=hatarf(index%8)
+        }
+        while (szamlalo<8 && szam+9<=64) {
+            szam=szam+9
+            szamok.push(szam)
+            szamlalo=szamlalo+1
+        }
+        return szamok
 
     }
-    Lepes_Lehetoseg(){
-        
+    lepesekbf(index){
+        szamlalo=hatarf(index%8)
+        if (szamlalo>hatarr(index/8)){
+            szamlalo=hatarr(index/8)
+        }
+        szam=index
+        while (szamlalo>0 && szam+7<64) {
+            szam=szam+7
+            szamok.push(szam)
+            szamlalo=szamlalo-1
+        }
+        return szamok
     }
+    lepesekjl(index){
+        szam=index
+        szamlalo=hatarr(index/8)
+        if (szamlalo>hatarf(index%8)){
+            szamlalo=hatarf(index%8)
+
+        }
+        console.log(szamlalo)
+        while (szamlalo<8 && szam-9>0) {
+            szam=szam-9
+            szamok.push(szam)
+            szamlalo=szamlalo+1
+        }
+        return szamok
+    }   
+    lepesekbl(index){
+        Szam=index
+        szamlalo=hatarr(index/8)
+        if (szamlalo<hatarf(index%8)){
+            szamlalo=hatarf(index%8)
+
+        }
+        while (szamlalo<8 && szam-7<64) {
+            szam=szam-7
+            szamok.push(szam)
+            szamlalo=szamlalo+1
+        }
+        return szamok
+    }
+
 }
 export default Futo
