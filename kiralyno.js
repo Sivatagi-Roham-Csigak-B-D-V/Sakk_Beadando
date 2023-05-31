aa28479c366bd97aa84239f148353800bf3fc0dd
dkornel01 <115635103+dkornel01@users.noreply.github.com>
2023-05-31 13:45:13 +0200

    kiralyno

--- kiralyno.js
+++ kiralyno.js
@@ -10,11 +10,158 @@ class Kiralyno{
         return "&#9819;"
         }
     }
-    Lepesek(){
-        
+    Lepesekf(index){
+        let szamok=[]
+        let szamlalo=index
+        while (szamlalo<64) {
+            szamok.push(szamlalo)
+            szamlalo=szamlalo+8
+        }
+        return szamok
     }
-    Lepes_Lehetoseg(){
-        
+    lepesj(index){
+        let szamok=[]
+        let szamlalo=index
+        while (szamlalo>0) {
+            szamok.push(szamlalo)
+            szamlalo=szamlalo-8
+        }
+        return szamok
+    }
+    lepesb(index){
+        let szamok=[]
+        let szamlalo=index
+        while(Lepes_Lehetoseg(index,szamlalo)){
+            szamok.push(szamlalo)
+            szamlalo=szamlalo+1
+         }
+         return szamok
+    }
+    lepesl(index){
+        let szamok=[]
+        let szamlalo=index
+        while(szamlalo>-4 && Lepes_Lehetoseg(index,szamlalo)){
+            szamok.push(szamlalo)
+            szamlalo=szamlalo-1
+        }
+        return szamok
+    }
+    
+    Lepes_Lehetoseg(index,szamlalo){
+        let sor=index/8
+        sor=hatarr(sor)
+        szamlalo=szamlalo/8
+        let viszintes=sor<=szamlalo
+        let viszintes2=sor+1>=szamlalo
+        return viszintes&& viszintes2
+    }
+    Lepesekjf(index){
+        let szamok=[]
+        let szamlalo=hatarr(index/8)
+        if (szamlalo>hatarf(index%8)){
+            szamlalo=hatarf(index%8)
+        }
+        while (szamlalo<8 && szam+9<=64) {
+            szam=szam+9
+            szamok.push(szam)
+            szamlalo=szamlalo+1
+        }
+        return szamok
+
+    }
+    lepesekbf(index){
+        szamlalo=hatarf(index%8)
+        if (szamlalo>hatarr(index/8)){
+            szamlalo=hatarr(index/8)
+        }
+        szam=index
+        while (szamlalo>0 && szam+7<64) {
+            szam=szam+7
+            szamok.push(szam)
+            szamlalo=szamlalo-1
+        }
+        return szamok
+    }
+    lepesekjl(index){
+        szam=index
+        szamlalo=hatarr(index/8)
+        if (szamlalo>hatarf(index%8)){
+            szamlalo=hatarf(index%8)
+
+        }
+        console.log(szamlalo)
+        while (szamlalo<8 && szam-9>0) {
+            szam=szam-9
+            szamok.push(szam)
+            szamlalo=szamlalo+1
+        }
+        return szamok
+    }   
+    lepesekbl(index){
+        Szam=index
+        szamlalo=hatarr(index/8)
+        if (szamlalo<hatarf(index%8)){
+            szamlalo=hatarf(index%8)
+
+        }
+        while (szamlalo<8 && szam-7<64) {
+            szam=szam-7
+            szamok.push(szam)
+            szamlalo=szamlalo+1
+        }
+        return szamok
+    }
+     hatarr(index){
+        if (index>7){
+            return 8
+        }
+        if (index>6){
+            return 7
+        }
+        if (index>5){
+            return 6
+        }
+        if (index>4){
+            return 5
+        }
+        if (index>3){
+            return 4
+        }
+        if (index>2){
+            return 3
+        }
+        if (index>1){
+            return 2
+        }
+        if (index>0){
+            return 1
+        }
+    }
+     hatarf(index){
+        if (index==7){
+            return 7
+        }
+        if (index==6){
+            return 5
+        }
+        if (index==5){
+            return 4
+        }
+        if (index==4){
+            return 3
+        }
+        if (index==3){
+            return 2
+        }
+        if (index==2){
+            return 1
+        }
+        if (index==1){
+            return 0
+        }
+        if (index==0){
+            return 8
+        }
     }
 }
 export default Kiralyno
